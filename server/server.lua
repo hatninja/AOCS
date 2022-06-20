d59f4f7836002cebc0bbc709ad1fd176b216947b
Jael Clark <jaelsonicboom@gmail.com>
2022-06-19 19:37:20 -0500

    Cleaned up server code.

--- server/server.lua
+++ server/server.lua
@@ -5,14 +5,14 @@ local MAXLISTEN = 128 --Maximum connected clients.
 local MAXSEND   = 2048 --Bytes to send per-update.
 local MAXREAD   = 2048 --Bytes to read per-update.
 
-function server.init(self)
+function server:init()
 	self.sockets = {}
 	self.closed = {}
 	self.buf = {}
 	self.got = {}
 end
 
-function server.listen(self, ip, port)
+function server:listen(ip, port)
 	local sock = socket.tcp()
 	sock:settimeout(0)
 	socket._SETSIZE = MAXLISTEN
@@ -22,33 +22,28 @@ function server.listen(self, ip, port)
 
 	self.socket = sock
 end
-function server.close(self)
+function server:close()
 	if not self.socket then return end
 	self.socket:close()
 	self.socket=nil
 end
 
-function server.send(self, sock, msg)
+function server:send(sock, msg)
 	self.buf[sock] = self.buf[sock] .. msg
 end
 
-function server.update(self)
+function server:update()
 	--Select sockets which are open to receiving/sending data.
 	local recv_t,send_t,err = socket.select(self.sockets,self.sockets, RATE)
 
 	--Read received client data.
 	for i,sock in ipairs(recv_t) do
 		local dat,err,part = sock:receive(MAXREAD)
-		self.got[sock] = self.got[sock] .. (dat or part)
-		if err == "closed" then
-			self.closed[sock] = true
-		end
+		self.got[sock]     = self.got[sock] .. (dat or part)
+		self.closed[sock]  = (err == "closed")
 	end
 
 	--Update clients.
-	for i,sock in ipairs(self.sockets) do
-		protocol:updateClient(sock)
-	end
 	for i,sock in ipairs(self.sockets) do
 		process:updateClient(sock)
 	end
@@ -57,27 +52,20 @@ function server.update(self)
 	for i,sock in ipairs(send_t) do
 		local dat = self.buf[sock]
 		if #dat > 0 then
-			local bytes,err = sock:send(dat,1,MAXSEND)
-			self.buf[sock] = string.sub(dat,bytes+1,-1)
-			if err == "closed" then
-				self.closed[sock] = true
-			end
+			local bytes,err   = sock:send(dat,1,MAXSEND)
+			self.buf[sock]    = string.sub(dat,bytes+1,-1)
+			self.closed[sock] = (err == "closed")
 		end
 	end
 
 	--Remove closed clients.
 	for sock in pairs(self.closed) do
-		for i,v in ipairs(self.sockets) do
-			if v == sock then
-				table.remove(self.sockets,i)
-				break
-			end
-		end
+		table.remove(self.sockets, find(self.sockets,sock) )
 
-		self.closed[sock] = nil
+		self.closed[sock]  = nil
 		self.sockets[sock] = nil
-		self.buf[sock] = nil
-		self.got[sock] = nil
+		self.buf[sock]     = nil
+		self.got[sock]     = nil
 
 		protocol:closeClient(sock)
 	end
@@ -87,12 +75,12 @@ function server.update(self)
 		repeat
 			local sock,err = self.socket:accept()
 			if sock then
+				sock:settimeout(0)
 				table.insert(self.sockets,sock)
 
-				sock:settimeout(0)
-				self.sockets[sock] = sock
-				self.buf[sock] = ""
-				self.got[sock] = ""
+				self.sockets[sock] = true
+				self.buf[sock]     = ""
+				self.got[sock]     = ""
 
 				protocol:acceptClient(sock)
 			end
