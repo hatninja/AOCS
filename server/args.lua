1dce31c6d2883f4da5e91cddfa05799fc41ca97b
Jael Clark <jaelsonicboom@gmail.com>
2022-08-06 00:51:08 -0500

    Launch options help.

--- server/args.lua
+++ server/args.lua
@@ -2,12 +2,21 @@
 --Read launch arguments to configure program state. Return true to cancel launch.
 return function(...)
 	for i,arg in ipairs{...} do
-		if arg == "monitor_all" then
-			monitor_web=true
-			monitor_ao=true
-			monitor_proc=true
-			monitor_sock=true
+		if arg == "?" or arg == "help" then
+			print([[AOCS Launch Options:
+debug        - Enable development tools.
+monitor_web  - View data received from websockets.
+monitor_ao   - View AO2 protocol data.
+monitor_proc - Track process actions.
+monitor_sock - Track connected sockets.]])
+			return true
 		end
-		_G[arg]=true
+		if arg == "debug"
+		or arg == "monitor_web"
+		or arg == "monitor_ao"
+		or arg == "monitor_proc"
+		or arg == "monitor_sock" then
+			_G[arg]=true
+		end
 	end
 end
